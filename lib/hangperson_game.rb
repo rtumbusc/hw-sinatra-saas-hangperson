6d5d14ed491e38d1838893eb81b223bb73bd3d01
rtumbusc <rtumbusc81@yahoo.com>
2015-09-30 05:01:33 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,16 +1,48 @@
 class HangpersonGame
+attr_accessor :word
+attr_accessor :guesses
+attr_accessor :wrong_guesses
+attr_accessor :wordGuesses
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
+def initialize(new_word)
+    @word = new_word
+    @guesses = ''
+    @wrong_guesses = ''
+    @wordGuesses = ''
+    for x in 0...new_word.length
+        @wordGuesses += '-'
+    end
+end
 
-  # Get a word from remote "random word" service
+def guess(letter)
+    if not letter =~ (/[^A-Za-z]/)
+        raise ArgumentError, "Your Input Is Not A Letter"
+    elsif self.guesses.include? letter or self.wrong_guesses.include? letter
+        false
+    elsif self.word.include? letter
+        add_correct(letter)
+        true
+    else
+        self.wrong_guesses = self.wrong_guesses + letter
+    end
+end
 
-  # def initialize()
-  # end
-  
-  def initialize(word)
-    @word = word
-  end
+def add_correct(letter)
+    indices = (0 ... self.word.length).find_all { |i| self.world[x,1] == letter }
+    indices.each do |x|
+        self.wordGuesses[x] = letter
+    end
+end
+
+def check_win_or_lose
+    if self.wrong_guesses.length > 6
+        :lose
+    elsif !self.wordGuesses.include? '-'
+        :win
+    else
+        :play
+    end
+end
 
   def self.get_random_word
     require 'uri'
